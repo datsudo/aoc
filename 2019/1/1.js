a1bd54440d0ad80336745781ca18201e97994171
datsudo <76833632+datsudo@users.noreply.github.com>
2024-11-11 11:59:26 +0800

    1

---
+++ 2019/1/1.js
@@ -0,0 +1,24 @@
+import fs from "fs/promises";
+
+let input = await fs.readFile("./1/input.txt", "utf-8")
+let modules = input.split("\n").map((i) => Number.parseInt(i)).slice(0, -1)
+
+let inputtest = [100756]
+
+function getTotalFuel(mass, accum = 0) {
+    let fuel = Math.floor(mass / 3) - 2
+    if (Math.floor(fuel / 3) <= 0) {
+        return accum + (fuel > 0 ? fuel : 0)
+    }
+    return getTotalFuel(fuel, accum + fuel)
+}
+
+function s1(modules) {
+    return modules.map((mass) => Math.floor(mass / 3) - 2).reduce((acc, curr) => acc + curr, 0)
+}
+
+function s2(modules) {
+    return modules.map((mass) => getTotalFuel(mass)).reduce((acc, curr) => acc + curr, 0)
+}
+
+console.log(s2(modules))
