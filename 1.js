4a388fa93b30b1bb07baf30ff6652adf5011e529
datsudo <76833632+datsudo@users.noreply.github.com>
2024-12-01 18:20:29 +0800

    1

--- 1.js
+++ 1.js
@@ -10,19 +10,6 @@ let rightDist = pairs.map(p => p[1])
 let leftSorted = leftDist.sort((a, b) => a - b)
 let rightSorted = rightDist.sort((a, b) => a - b)
 
-
-function s1() {
-    return leftSorted
-        .map((l, i) => Math.abs(l - rightSorted[i]))
-        .reduce((acc, curr) => acc + curr, 0)
-}
-
-function s2() {
-    return leftDist
-        .map(l => l * rightDist.filter(r => r == l).length)
-        .reduce((acc, curr) => acc + curr, 0)
-}
-
 console.log(
     leftSorted
         .map((l, i) => Math.abs(l - rightSorted[i]))
